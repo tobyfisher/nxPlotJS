981bfff58ae2ca156125457a7e39e19777f81fbc
Toby Fisher <toby@knowego.com>
2024-02-03 21:52:54 +0000

    reviewing code

--- src/app.js
+++ src/app.js
@@ -14,52 +14,57 @@ debug.log(`nxPlot is available - Plot.ly v${Plotly.version}`);
  * Note: if User changes the theme generate a broadcast Event to let nxPlot know about it
  */
 
-const templates = new Map();
+const allowedTemplates = new Set([
+	"barChart",
+	"eyesOutcomes_Errors",
+	"eyesOutcomes_offScale_selectableVA",
+	"outcomes_Errors",
+	"splitRL_Adherence",
+	"splitRL_Glaucoma_selectableVA",
+	"splitRL_MedicalRetina_selectableVA"
+]);
 
-// data all on one single plot
-templates.set("barChart", layouts.barChart);
-templates.set("eyesOutcomes_Errors", layouts.eyesOutcome_Errors);
-templates.set("eyesOutcomes_offScale_selectableVA", layouts.eyesOutcomes_offScale_selectableVA);
-templates.set("outcomes_Errors", layouts.outcomes_Errors);
-
-// split data into 2 different plots for Right and Left eye
-templates.set("splitRL_Adherence", layouts.splitRL_Adherence);
-templates.set("splitRL_Glaucoma_selectableVA", layouts.splitRL_Glaucoma_selectableVA);
-templates.set("splitRL_MedicalRetina_selectableVA", layouts.splitRL_MedicalRetina_selectableVA);
+const plotTemplates = new Map();
+for( const template of allowedTemplates){
+	plotTemplates.set(template, layouts[template]);
+}
 
 const nxPlot = ( requestedPlotLayout, divID = false ) => {
 
-	if ( templates.has(requestedPlotLayout) ){
-
-		const nxLayout = templates.get(requestedPlotLayout);
+	try {
+		const nxLayout = plotTemplates.get(requestedPlotLayout);
 		nxLayout.setPlotlyDiv(divID);
+
 		/**
-		 * Some templates require setting up, this could
+		 * Some plotTemplates require setting up, this could
 		 * be exposed as an API with options but for now
 		 * handle internally
 		 */
 		nxLayout.setup();
+		debug.log(`Building: ${ requestedPlotLayout}`);
 
 		/**
-		 * Users changes the theme
+		 * If users changes the theme
 		 * re-build to re-draw plot with correct colours
 		 */
 		document.addEventListener('oeThemeChange', () => {
 			nxLayout.plotlyThemeChange();
 		});
 
-		debug.log(`Building: ${ requestedPlotLayout}`);
-
 		return nxLayout;
 
-	} else {
-		console.error(`Requested plot template: "${requestedPlotLayout}" is invalid.
-			Valid templates are: ${Array.from(templates.keys()).join(", ")}`);
+	} catch ({ name }){
+		console.error( name );
+		console.error(`Requested plot template: "${requestedPlotLayout}" is invalid. 
+		Valid plotTemplates are:\n${Array.from(plotTemplates.keys()).join("\n")}` );
 
 		return false;
 	}
 }
 
+/**
+ * Public API
+ */
 Object.defineProperty(window, 'nxPlot', {
 	value: nxPlot,
 	writable: false
