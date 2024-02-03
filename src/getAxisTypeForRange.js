7ca37cd1061932b56919cb98dab0d7e39b8c3914
Toby Fisher <toby@knowego.com>
2024-02-03 22:01:40 +0000

    reviewing code

--- src/getAxisTypeForRange.js
+++ src/getAxisTypeForRange.js
@@ -1,20 +1,20 @@
-export const getAxisTypeForRange = ( range ) => {
-	/**
-	 * Based on the unit range type build axis
-	 * Could be a number range or categories
-	 */
-	if ( typeof range[0] === 'number' ){
-		return {
-			range, // e.g. [n1, n2];
-			axisType: "linear", // set the axis.type explicitly here
-		};
-	} else {
-		return {
-			useCategories: {
-				showAll: true,
-				categoryarray: range
-			}
-		};
+const numberRange = ( range ) => ({
+	range, // e.g. [n1, n2];
+	axisType: "linear", // set the axis.type explicitly here
+});
+
+const categoryRange = ( range ) => ({
+	useCategories: {
+		showAll: true,
+		categoryarray: range
 	}
+});
+
+/**
+ * Based on the unit range type build axis
+ * Could be a number range or categories
+ */
+export const getAxisTypeForRange = ( range ) => {
+	return typeof range[0] === 'number' ? numberRange( range ) : categoryRange( range );
 }
 
